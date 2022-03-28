b32debc41dc4970c95e0bbd9c19303cb61139bef
ingrid-inga <leidyingrid20@gmail.com>
2022-03-27 21:20:12 -0300

    Solucion ejercicios Unidad 2

--- bootcampjs/script.js
+++ bootcampjs/script.js
@@ -1,1 +1,39 @@
 console.log('Hello World! 👋')
+
+//Ejercicios Practicos Nº2-1
+
+//1.-Declarar una variable de nombre cantidadDeVentanasEnMiCasa y asignarle la cantidad de ventanas 
+//que hay en tu casa. Mostrar el valor de la variable por la consola.
+
+var cantidadDeVentanasEnMiCasa = 8
+console.log(cantidadDeVentanasEnMiCasa)
+
+//2.-Declarar una variable de nombre soyHumano y asignarle el valor true (verdadero).
+// Mostrar el valor de la variable por la consola.
+
+var soyHumano = true
+console.log(soyHumano)
+
+//3.-Declarar una variable de nombre miGustoDePizzaPreferido y asignarle la variedad 
+//de pizza que más te guste usando un String (cadena de caracteres). Mostrar el valor de la variable por la consola.
+
+var miGustoDePizzaPreferido = 'hawaiana'
+console.log(miGustoDePizzaPreferido)
+
+//4.-Mostrar por la consola los siguientes valores false, 'Hola', 0 y 'Minions' usando la misma variable.
+
+var values = [false, 'Hola', 0 , 'Minions']
+console.log(values)
+
+var value = false
+console.log(value)
+
+var value = 'Hola'
+console.log(value)
+
+var value = 0
+console.log(value)
+
+var value = 'Minions'
+console.log(value)
+
